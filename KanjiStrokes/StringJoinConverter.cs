a7cd0b4b3360bad375d4eb6b32d75c45ba2cad7f
Mario Welzig <welzig@frucost.net>
2020-05-09 20:46:04 +0200

    show nanori in info panel

---
+++ KanjiStrokes/StringJoinConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace KanjiStrokes {
+	[ValueConversion(typeof(IEnumerable<string>), typeof(string))]
+	class StringJoinConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			if(!(value is IEnumerable<string> strings)) return "";
+			return string.Join(parameter as string ?? "", strings);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			throw new NotImplementedException();
+		}
+	}
+}
